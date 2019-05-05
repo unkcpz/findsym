6b576ef787235a4211aefed7d90fc5022336c811
Jason.Yu <morty.yu@yahoo.com>
2019-05-05 16:47:42 +0800

    alpha

--- main.go
+++ main.go
@@ -1,25 +1,61 @@
 package main
 
 import (
-  "io/ioutil"
-  "fmt"
+    "fmt"
+    "log"
+    "net/http"
+    "strconv"
 
-  cmpio "github.com/unkcpz/gocmp/io"
-  "github.com/unkcpz/gocmp/crystal"
-  "github.com/unkcpz/spglib/go/spglib"
-  "gonum.org/v1/gonum/mat"
+    cmpio "github.com/unkcpz/gocmp/io"
+    "github.com/unkcpz/gocmp/crystal"
+    "github.com/unkcpz/spglib/go/spglib"
+    "gonum.org/v1/gonum/mat"
 )
+func proc(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    switch r.Method {
+    case "GET":
+         http.ServeFile(w, r, "form.html")
+    case "POST":
+        // Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+        if err := r.ParseForm(); err != nil {
+            fmt.Fprintf(w, "ParseForm() err: %v", err)
+            return
+        }
+        fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+        poscar := r.FormValue("poscar")
+        symprecStr := r.FormValue("symprec")
+        symprec, err := strconv.ParseFloat(symprecStr, 64)
+        fmt.Fprintf(w, "Poscar = %s\n", poscar)
+        fmt.Fprintf(w, "Symprec = %s\n", symprecStr)
+
+        symdata, err := findsym(poscar, symprec)
+        if err != nil {
+          fmt.Fprintf(w, "findsym(%s, %v) err: %v", poscar, symprec, err)
+        }
+        fmt.Fprintf(w, "Spacegroup symbol: %s", symdata)
+    default:
+        fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+    }
+}
 
 func main() {
-  data, err := ioutil.ReadFile("./POSCAR")
-  if err != nil {
-    panic(err)
-  }
+    http.HandleFunc("/", proc)
 
-  s := string(data)
-  poscarCell, err := cmpio.ParsePoscar(s)
+    fmt.Printf("Starting server for testing HTTP POST...\n")
+    if err := http.ListenAndServe(":6116", nil); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func findsym(poscar string, eps float64) (string, error) {
+  poscarCell, err := cmpio.ParsePoscar(poscar)
   if err != nil {
-    panic(err)
+    return "", err
   }
 
   lattice := poscarCell.Lattice
@@ -34,16 +70,16 @@ func main() {
   }
   cell, err := crystal.NewCell(lattice, positions, types, c)
   if err != nil {
-    panic(err)
+    return "", err
   }
 
   ds := spglib.NewDataset(
     MatData(*cell.Lattice),
     MatData(*cell.Positions),
     cell.Types,
-    1e-5,
+    eps,
   )
-  fmt.Printf("SpaceSymbol: %s\n", ds.SpaceSymbol)
+  return ds.SpaceSymbol, nil
 }
 
 func MatData(mat mat.Dense) []float64 {
